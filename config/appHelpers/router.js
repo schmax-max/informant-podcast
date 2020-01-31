0ff29ca2acf89ac9420a74d8646489479aeedb52
aschmach <anders.schmachtenberg@gmail.com>
2020-01-31 13:26:20 +1100

    changed to use parameters instead of body on routing

--- config/appHelpers/router.js
+++ config/appHelpers/router.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const {master} = require('../../svc');
 const service = require('../service');
 
-router.post(`/${service}`, trigger);
+router.post(`/${service}/:type`, trigger);
 
 async function trigger (req, res, next) {
-  const response = await master(req.body)
+  const response = await master(req.params)
   res.send(response)
 }
 
